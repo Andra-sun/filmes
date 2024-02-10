60b8d95587966ebc1a2323beca845e6a6e2432b1
Andra <111245270+Andra-sun@users.noreply.github.com>
2024-02-10 08:13:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,45 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var container = document.getElementById("minhaLista");
+document.getElementById("irSenha").addEventListener("click", function () {
+    var senha = document.getElementById("senha").value.trim().toLowerCase();
+    var aSenha = "pitty";
+    var tela = document.getElementById("tela");
+    var inicio = document.getElementById("inicio");
 
-    function carregarJSON() {
-        return new Promise((resolve, reject) => {
-            fetch("filmes.json")
-                .then((response) => response.json())
-                .then((data) => resolve(data))
-                .catch((error) => reject(error));
-        });
+    if (senha === aSenha) {
+        alert("Acesso liberado!");
+        tela.style.display = "flex";
+        inicio.style.display = "none";
+    } else {
+        alert("Acesso negado >:(");
     }
-
-    carregarJSON()
-        .then((data) => {
-            console.log("Dados do JSON:", data);
-
-            data.itens.forEach(function (item) {
-                var div = document.createElement("div");
-                div.id = `item-${item.id}`;
-                div.className = "base";
-
-                var notao = document.createElement("div");
-                notao.id = "notao";
-                div.appendChild(notao);
-
-                var img = document.createElement("img");
-                img.src = item.poster;
-                div.appendChild(img);
-
-                var subDiv = document.createElement("div");
-                subDiv.className = "subBase";
-                div.appendChild(subDiv);
-
-                var p1 = document.createElement("p");
-                p1.textContent = item.nome;
-                p1.id = "titulo";
-                subDiv.appendChild(p1);
-
-                var p2 = document.createElement("p");
-                p2.textContent = item.nota;
-                p2.id = "nota";
-                if (item.nota <= 40) {
-                    p2.style.backgroundColor = "red";
-                }
-                if (item.nota == 50 || 60) {
-                    p2.style.backgroundColor = "yellow";
-                }
-                if (item.nota >= 70) {
-                    p2.style.backgroundColor = "green";
-                }
-                notao.appendChild(p2);
-
-                container.appendChild(div);
-            });
-        })
-        .catch((error) => console.error("Erro ao carregar JSON:", error));
 });
+/* 
+document
+    .getElementById("adicionarFilme")
+    .addEventListener("click", function () {
+        const titulo = document.getElementById("tituloInput").value.trim();
+        const nota = parseInt(document.getElementById("notaInput").value);
+
+        if (titulo === "" || isNaN(nota)) {
+            alert("Por favor, preencha todos os campos corretamente.");
+            return;
+        }
+
+        const novoFilme = {
+            nome: titulo,
+            nota: nota,
+        };
+
+        fetch("filmes.json", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(novoFilme),
+        })
+            .then((response) => response.text())
+            .then((message) => {
+                console.log(message);
+            })
+            .catch((error) => console.error("Erro ao adicionar filme:", error));
+    });
+ */
